9c0ab79e15649f2de08668c753ac69ebcf487fea
YousraChahinez <yousrachahinez30@gmail.com>
2025-05-06 07:56:46 +0100

    add exo5.c and update...

--- TPs/TP2/exo5.c
+++ TPs/TP2/exo5.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Contact {
     char Fname[20];
@@ -10,45 +11,92 @@ struct Contact {
 struct Contact c;
 FILE *fptr;
 
-void add(){
-    fptr = fopen("file.txt", "w");
+void add() {
+    fptr = fopen("contacts.txt", "a");  // Use "a" mode to append instead of overwrite
     
-    if (fptr != NULL){
-        printf("Entre first, last name, and phone: ");
-        scanf("%s", &c.Fname);
-        scanf("%s", &c.Lname);
-        scanf("%s", &c.Phone);
+    if (fptr == NULL) {
+        printf("Failed to open file!\n");
+        return;
+    }
+    
+    printf("Enter first name: ");
+    scanf("%s", c.Fname);
+    printf("Enter last name: ");
+    scanf("%s", c.Lname);
+    printf("Enter phone: ");
+    scanf("%s", c.Phone);
 
-        fprintf(fptr, "%s ", c.Fname);
-        fprintf(fptr, "%s ", c.Lname);
-        fprintf(fptr, "%s ", c.Phone);
-
-        fclose(fptr);
-    } else {
-        printf("Creation failed...");
-    }   
+    fprintf(fptr, "%s %s %s\n", c.Fname, c.Lname, c.Phone);
+    fclose(fptr);
+    printf("Contact added successfully!\n");
 }
 
-void display(){
-    fptr = fopen("file.txt", "r");
-
-    if (fptr != NULL){
-        fscanf(fptr, "%s", &c.Fname);
-        fscanf(fptr, "%s", &c.Lname);
-        fscanf(fptr, "%s", &c.Phone);
-        printf("%s", c.Fname);
-        printf("%s", c.Lname);
-        printf("%s", c.Phone);
-    } else {
-        printf("File doesn't exist!");
+void display() {
+    fptr = fopen("contacts.txt", "r");
+    
+    if (fptr == NULL) {
+        printf("No contacts found!\n");
+        return;
     }
-
+    
+    printf("\n--- All Contacts ---\n");
+    while(fscanf(fptr, "%s %s %s", c.Fname, c.Lname, c.Phone) != EOF) {
+        printf("First: %s\n", c.Fname);
+        printf("Last: %s\n", c.Lname);
+        printf("Phone: %s\n\n", c.Phone);
+    }
     fclose(fptr);
 }
 
-int main ()
-{
-    // add();
-    display();
+void search() {
+    char searchName[20];
+    int found = 0;
+    
+    printf("Enter first name to search: ");
+    scanf("%s", searchName);
+    
+    fptr = fopen("contacts.txt", "r");
+    if (fptr == NULL) {
+        printf("No contacts found!\n");
+        return;
+    }
+    
+    printf("\n--- Search Results ---\n");
+    while(fscanf(fptr, "%s %s %s", c.Fname, c.Lname, c.Phone) != EOF) {
+        if(strcmp(c.Fname, searchName) == 0) {
+            printf("First: %s\n", c.Fname);
+            printf("Last: %s\n", c.Lname);
+            printf("Phone: %s\n\n", c.Phone);
+            found = 1;
+        }
+    }
+    
+    if (!found) {
+        printf("No contacts matching '%s' found.\n", searchName);
+    }
+    fclose(fptr);
+}
+
+int main() {
+    int choice;
+    
+    do {
+        printf("\nContact Management System\n");
+        printf("1. Add Contact\n");
+        printf("2. Display All Contacts\n");
+        printf("3. Search Contact\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        
+        switch(choice) {
+            case 1: add(); break;
+            case 2: display(); break;
+            case 3: search(); break;
+            case 4: printf("Goodbye!\n"); break;
+            default: printf("Invalid choice!\n");
+        }
+    } while(choice != 4);
+    
     return 0;
 }
